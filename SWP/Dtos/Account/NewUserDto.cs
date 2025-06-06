b3bc11af9ee53743e95c55866538a79a4d95f099
NDBao <baondse182513@fpt.edu.vn>
2025-06-06 12:38:42 +0700

    FR11-Register
    API-Register(Username, Password, phone, mail)

--- SWP/Dtos/Account/NewUserDto.cs
+++ SWP/Dtos/Account/NewUserDto.cs
@@ -2,6 +2,10 @@
 {
     public class NewUserDto
     {
-        
+        public string UserName { get; set; }
+
+        public string Role { get; set; }
+
+        public string Token { get; set; }
     }
 }
---
+++ SWP/Service/TokenService.cs
@@ -0,0 +1,44 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using Microsoft.IdentityModel.Tokens;
+using SWP.Interfaces;
+
+namespace SWP.Service
+{
+        public class TokenService : ITokenService
+        {
+
+            private readonly IConfiguration _config;
+            private readonly SymmetricSecurityKey _key;
+
+            public TokenService(IConfiguration config)
+            {
+                _config = config;
+                _key = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes(_config["JWT:Signingkey"]));
+            }
+            public string CreateToken(string accName, int accId, string roleName)
+            {
+                var claims = new List<Claim>
+                {
+                    new Claim(JwtRegisteredClaimNames.NameId, accName),
+                    new Claim(JwtRegisteredClaimNames.Sub, accId.ToString()),
+                    new Claim(ClaimTypes.Role, roleName)
+                };
+
+                var creds = new SigningCredentials(_key, SecurityAlgorithms.HmacSha256Signature);
+
+                var tokenDescriptor = new SecurityTokenDescriptor
+                {
+                    Subject = new ClaimsIdentity(claims),
+                    Expires = DateTime.Now.AddDays(7),
+                    SigningCredentials = creds,
+                    Issuer = _config["JWT:Issuer"],
+                    Audience = _config["JWT:Audience"]
+                };
+
+                var tokenHandler = new JwtSecurityTokenHandler();
+                var token = tokenHandler.CreateToken(tokenDescriptor);
+                return tokenHandler.WriteToken(token);
+            }
+        }
+}
