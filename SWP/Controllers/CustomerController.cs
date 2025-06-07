03765dd16f8a733bb079fb6cb87ad76172f64b16
J2T02 <loctxse182603@fpt.edu.vn>
2025-06-07 22:51:17 +0700

    Customer API
    List customer

---
+++ SWP/Controllers/CustomerController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SWP.Interfaces;
+using SWP.Mapper;
+using SWP.Models;
+
+namespace SWP.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+        private readonly ICustomerRepository _customerRepository;
+        private readonly HIEM_MUONContext _context;
+        public CustomerController(ICustomerRepository customerRepository, HIEM_MUONContext context)
+        {
+            _customerRepository = customerRepository;
+            _context = context;
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetAllCustomers()
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            var customers = await _customerRepository.GetAllCustomersAsync();
+            var customerDto = customers.Select(c => c.ToCustomerDto());
+             return Ok(customers);
+        }
+    }
+}
---
+++ SWP/Repository/CustomerRepository.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SWP.Interfaces;
+using SWP.Models;
+
+namespace SWP.Repository
+{
+    public class CustomerRepository : ICustomerRepository
+    {
+        private readonly HIEM_MUONContext _context;
+        public CustomerRepository(HIEM_MUONContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<Customer>> GetAllCustomersAsync()
+        {
+           return await _context.Customers.ToListAsync();
+        }
+
+        public async Task<Customer?> GetCustomerByIdAsync(int id)
+        {
+            return await _context.Customers.FirstOrDefaultAsync(c => c.CusId == id);
+        }
+        // Implement methods for customer repository here\
+
+    }
+    
+    
+}
