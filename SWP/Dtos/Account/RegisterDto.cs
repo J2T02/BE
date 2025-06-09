3dda6d6dbe677ca6632b55e36a486bc4bbda8221
NDBao <baondse182513@fpt.edu.vn>
2025-06-09 21:12:40 +0700

    Update Register - API
    update validation

--- SWP/Dtos/Account/RegisterDto.cs
+++ SWP/Dtos/Account/RegisterDto.cs
@@ -7,9 +7,15 @@ namespace SWP.Dtos.Account
         [Required]
 
         public string AccName { get; set; } = string.Empty;
+
+        [Required]
+        [MinLength(8, ErrorMessage = "Mật khẩu cần từ 8 chữ số trở lên vui lòng thử lại.")]
         public string Password { get; set; } = string.Empty;
-        
+
+        [Required]
+        [MinLength(10, ErrorMessage = "Sai số điện thoại.")]
         public string? Phone { get; set; }
+
         public string? Mail { get; set; }
     }
 }
