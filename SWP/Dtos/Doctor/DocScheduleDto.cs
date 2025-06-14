55c3e581c31743d776d2884f06ea746728380413
J2T02 <loctxse182603@fpt.edu.vn>
2025-06-14 12:40:34 +0700

    FR25 detail of booking

---
+++ SWP/Dtos/Doctor/DocScheduleDto.cs
@@ -0,0 +1,11 @@
+﻿namespace SWP.Dtos.Doctor
+{
+    public class DocScheduleDto
+    {
+        public DateOnly? Date { get; set; }
+        public TimeOnly? Start { get; set; }
+        public TimeOnly? End { get; set; }
+        public string Room { get; set; }
+
+    }
+}
---
+++ SWP/Dtos/Payment/PaymentDto.cs
@@ -0,0 +1,10 @@
+﻿namespace SWP.Dtos.Payment
+{
+    public class PaymentDto
+    {
+        public decimal Amount { get; set; }
+        public DateTime PaymentDate { get; set; }
+        public string Method { get; set; }
+        public string Status { get; set; }
+    }
+}
