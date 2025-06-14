79180bca99e274617e6f8dd0b9c7c1e63c91997c
Le Minh An <anlmse182525@fpt.edu.vn>
2025-06-14 12:47:23 +0700

    Register Doctor Schedule
    Doctor Schedule

---
+++ SWP/Dtos/Doctor/DoctorScheduleDto.cs
@@ -0,0 +1,15 @@
+﻿namespace SWP.Dtos.Doctor
+{
+    public class DoctorScheduleDto
+    {
+        public int DsId { get; set; }
+
+        public int? DocId { get; set; }
+
+        public DateOnly? WorkDate { get; set; }
+
+        public int? SlotId { get; set; }
+
+        public bool? IsAvailable { get; set; }
+    }
+}
