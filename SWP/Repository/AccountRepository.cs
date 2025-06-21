6312261db2b9b7155f89473888d88133bfe1fdb2
NDBao <baondse182513@fpt.edu.vn>
2025-06-21 11:34:59 +0700

    them lay account

---
+++ SWP/Repository/AccountRepository.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SWP.Interfaces;
+using SWP.Models;
+
+namespace SWP.Repository
+{
+    public class AccountRepository : IAccountRepository
+    {
+        private readonly HIEM_MUONContext _context;
+
+        
+        public AccountRepository(HIEM_MUONContext context)
+        {
+            _context = context;
+        }
+        public async Task<Account?> GetAccountAsync(int accId)
+        {
+            // Assuming you want to return the first account or a specific one
+            return await _context.Accounts.FirstOrDefaultAsync(a => a.AccId == accId);
+        }
+    }
+    
+}
