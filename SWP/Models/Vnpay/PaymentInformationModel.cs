e8d153e5dfa35e8c07b3be35f8748c4bc181ba79
NDBao <baondse182513@fpt.edu.vn>
2025-07-04 00:07:18 +0700

    vnpay

---
+++ SWP/Models/Vnpay/PaymentInformationModel.cs
@@ -0,0 +1,12 @@
+﻿namespace SWP.Models.Vnpay
+{
+    public class PaymentInformationModel
+    {
+        public int OrderId { get; set; }
+        public string OrderType { get; set; }
+        public decimal Amount { get; set; }
+        public string OrderDescription { get; set; }
+        public string Name { get; set; }
+
+    }
+}
