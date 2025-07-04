4718ea80667a3a1b6b569068db983cc2578d050d
J2T02 <loctxse182603@fpt.edu.vn>
2025-07-04 13:24:28 +0700

    FR43 Get FeedBack by DocId or TPId

---
+++ SWP/Controllers/FeedbackController.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SWP.Data;
+using SWP.Dtos.Feedback;
+using SWP.Interfaces;
+using SWP.Mapper;
+using System.Net;
+
+namespace SWP.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FeedbackController : ControllerBase
+    {
+        private readonly IFeedbackRepository _feedbackRepository;
+
+        public FeedbackController(IFeedbackRepository feedbackRepository)
+        {
+            _feedbackRepository = feedbackRepository;
+        }
+
+        [HttpGet("doctor/{doctorId}")]
+        public async Task<BaseRespone<List<FeedbackDto>>> GetFeedbacksByDoctorId(int doctorId)
+        {
+            try
+            {
+                var feedbacks = await _feedbackRepository.GetFeedbacksByDoctorIdAsync(doctorId);
+
+                if (feedbacks == null || !feedbacks.Any())
+                {
+                    return new BaseRespone<List<FeedbackDto>>(
+                        statusCode: HttpStatusCode.NotFound,
+                        message: "Không tìm thấy Feedback cho Doctor này"
+                    );
+                }
+
+                var feedbackDtos = FeedbackMapper.ToDtoList(feedbacks);
+
+                return new BaseRespone<List<FeedbackDto>>(
+                    data: feedbackDtos,
+                    message: "Lấy dữ liệu thành công",
+                    statusCode: HttpStatusCode.OK
+                );
+            }
+            catch (Exception ex)
+            {
+                return new BaseRespone<List<FeedbackDto>>(
+                    statusCode: HttpStatusCode.InternalServerError,
+                    message: "Lỗi: " + ex.Message
+                );
+            }
+        }
+
+        [HttpGet("TreatmentPlan/{treatmentPlanId}")]
+        public async Task<BaseRespone<List<FeedbackDto>>> GetFeedbacksByTreatmentPlanId(int treatmentPlanId)
+        {
+            try
+            {
+                var feedbacks = await _feedbackRepository.GetFeedbacksByTreatmentPlanIdAsync(treatmentPlanId);
+
+                if (feedbacks == null || !feedbacks.Any())
+                {
+                    return new BaseRespone<List<FeedbackDto>>(
+                        statusCode: HttpStatusCode.NotFound,
+                        message: "Không tìm thấy feedback của TreatmentPlan này"
+                    );
+                }
+
+                var feedbackDtos = FeedbackMapper.ToDtoList(feedbacks);
+
+                return new BaseRespone<List<FeedbackDto>>(
+                    data: feedbackDtos,
+                    message: "Lấy dữ liệu thành công",
+                    statusCode: HttpStatusCode.OK
+                );
+            }
+            catch (Exception ex)
+            {
+                return new BaseRespone<List<FeedbackDto>>(
+                    statusCode: HttpStatusCode.InternalServerError,
+                    message: "Lỗi: " + ex.Message
+                );
+            }
+        }
+
+    }
+}
---
+++ SWP/Mapper/FeedbackMapper.cs
@@ -0,0 +1,26 @@
+﻿using SWP.Dtos.Feedback;
+using SWP.Models;
+
+namespace SWP.Mapper
+{
+    public class FeedbackMapper
+    {
+        public static FeedbackDto ToDto(Feedback feedback)
+        {
+            return new FeedbackDto
+            {
+                DoctorId = feedback.DocId ?? 0,
+                TreatmentPlanId = feedback.TpId ?? 0,
+                star = feedback.Star,
+                CreateAt = feedback.CreateAt ?? DateOnly.MinValue,
+                Content = feedback.Content ?? string.Empty,
+                Cus = feedback.Tp.Cus != null ? CustomerMapper.ToCustomerDto(feedback.Tp.Cus) : null
+            };
+        }
+
+        public static List<FeedbackDto> ToDtoList(IEnumerable<Feedback> feedbacks)
+        {
+            return feedbacks.Select(f => ToDto(f)).ToList();
+        }
+    }
+}
