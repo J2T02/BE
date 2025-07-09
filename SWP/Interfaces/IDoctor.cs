f2775de49bfa3188cf4b80fa07a0878f8fc28ee3
Le Minh An <anlmse182525@fpt.edu.vn>
2025-07-10 02:32:55 +0700

    Fix Doctor

--- SWP/Interfaces/IDoctor.cs
+++ SWP/Interfaces/IDoctor.cs
@@ -8,7 +8,7 @@ namespace SWP.Interfaces
         Task<List<Doctor>> GetAllDoctorsAsync();
         Task<Doctor> GetDoctorByIdAsync(int id);
         Task<Account> GetAccountByDoctor(Doctor doctor);
-        Task<Doctor> GetDoctorByAccountId(int accountId);
+        Task<Doctor?> GetDoctorByAccountId(int accountId);
         Task<Doctor> CreateDoctorAsync(Doctor doctor);
         Task<Doctor> UpdateDoctorAsync(int id, UpdateDoctorRequestDto doctor);
         Task<string> DeleteDoctorAsync(int id);
