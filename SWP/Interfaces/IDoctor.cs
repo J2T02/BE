6ad0091f3c9622ddaa98730e6e577a95b11438ee
NDBao <baondse182513@fpt.edu.vn>
2025-07-09 15:29:22 +0700

    getdocbyAccId

--- SWP/Interfaces/IDoctor.cs
+++ SWP/Interfaces/IDoctor.cs
@@ -18,5 +18,7 @@ namespace SWP.Interfaces
         Task<List<DoctorSchedule>> GetAllDoctorSchedule(int doctorId);
 
         Task<SlotSchedule?> GetSlotById(int id);
+
+        Task<Doctor> GetDoctorByAccountIdAsync(int accountId);
     }
 }
